fba944729627219695812109e921ab62c5283626
kelvinhenri99 <kelvin5henri@gmail.com>
2023-08-01 18:55:58 -0300

    feat: create components default

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,47 +1,53 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import Menu from './components/Menu.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <main>
-    <TheWelcome />
+    <Menu />
+    <router-view />
   </main>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  body {
+    margin: 0 auto;
+    padding: 0px;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  .titleMain {
+    color: rgb(94, 231, 30);
+    font-size: 27px;
+    text-align: center;
+    padding: 30px 0px;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .containerApp {
+    border: 1px solid rgb(94, 231, 30);
+    width: 1200px;
+    margin: 0 auto;
+    min-height: 300px;
+    border-radius: 7px;
   }
-}
+  .containerApp h1 {
+    color: rgb(94, 231, 30);
+    font-size: 27px;
+    text-align: center;
+    padding: 30px 0px;
+    clear: both;
+  }
+  .containerApp h2 {
+    font-size: 24px;
+    text-align: center;
+    color: white;
+  }
+  .containerApp p {
+    font-size: 22px;
+    text-align: justify;
+  }
+  @media (max-width: 1190px) {
+    .containerApp {
+      width: 98%;
+    }
+  }
 </style>
---
+++ front-end/src/components/Menu.vue
@@ -0,0 +1,65 @@
+<script>
+
+</script>
+
+<template>
+  <div class="menu">
+    <router-link to="/" class="gridMenu">
+      <ion-icon name="home"></ion-icon>
+      <p>Início</p>
+    </router-link>
+    <router-link to="/login" class="gridMenu">
+      <ion-icon name="person"></ion-icon>
+      <p>Entrar</p>
+    </router-link>
+    <router-link to="/register" class="gridMenu">
+      <ion-icon name="create"></ion-icon>
+      <p>Cadastrar-se</p>
+    </router-link>
+    <router-link to="/preferences" class="gridMenu">
+      <ion-icon name="options"></ion-icon>
+      <p>Preferências</p>
+    </router-link>
+    <router-link to="/administrator" class="gridMenu">
+      <ion-icon name="server"></ion-icon>
+      <p>Administrador</p>
+    </router-link>
+    <router-link to="/logout" class="gridMenu">
+      <ion-icon name="close-circle"></ion-icon>
+      <p>Sair</p>
+    </router-link>
+  </div>
+</template>
+
+<style>
+  .menu {
+    width: 100%;
+    background-color: rgb(0, 0, 0);
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+    box-shadow: 0px 0px 50px black;
+  }
+  .gridMenu {
+    text-decoration: none!important;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: 15px;
+    margin-right: 15px;
+    margin-top: 15px;
+  }
+  .gridMenu ion-icon {
+    color: white;
+    font-size: 25px;
+    transition: 0.4s ease-in-out;
+  }
+  .gridMenu:hover ion-icon {
+    color: rgb(61, 209, 61);
+  }
+  .gridMenu p {
+    color: rgb(61, 209, 61);
+    text-decoration: none;
+  }
+</style>
